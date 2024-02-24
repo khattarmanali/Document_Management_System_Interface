d6463e6744cafc81209d0611f56d386512030c14
khattarmanali <manalikhattar1@gmail.com>
2024-02-24 10:53:24 +0530

    component refactor

--- src/components/alert/AppAlert.tsx
+++ src/components/alert/AppAlert.tsx
@@ -1,29 +1,20 @@
-import React from 'react';
-import { Alert, AlertProps, Snackbar, IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
+import { Alert, Snackbar, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
-type AlertStatus = 'info' | 'success' | 'warning' | 'error';
-
-interface CustomAlertProps {
-  open: boolean;
-
-  status: AlertStatus;
-  message: string;
-}
-
-const AppAlert: React.FC<CustomAlertProps> = ({ open, status, message }) => {
-  const getSeverity = (): AlertProps['severity'] => {
+const AppAlert = ({ open, status, message }) => {
+  const getSeverity = () => {
     switch (status) {
-      case 'info':
-        return 'info';
-      case 'success':
-        return 'success';
-      case 'warning':
-        return 'warning';
-      case 'error':
-        return 'error';
+      case "info":
+        return "info";
+      case "success":
+        return "success";
+      case "warning":
+        return "warning";
+      case "error":
+        return "error";
       default:
-        return 'info';
+        return "info";
     }
   };
 
@@ -31,17 +22,17 @@ const AppAlert: React.FC<CustomAlertProps> = ({ open, status, message }) => {
     <Snackbar
       open={open}
       autoHideDuration={6000} // Adjust as needed
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
     >
-      <Alert severity={getSeverity()} sx={{ width: '100%' }}>
+      <Alert severity={getSeverity()} sx={{ width: "100%" }}>
         {message}
         <IconButton
-          size='small'
-          aria-label='close'
-          color='inherit'
-          sx={{ position: 'absolute', right: 8, top: 8 }}
+          size="small"
+          aria-label="close"
+          color="inherit"
+          sx={{ position: "absolute", right: 8, top: 8 }}
         >
-          <CloseIcon fontSize='small' />
+          <CloseIcon fontSize="small" />
         </IconButton>
       </Alert>
     </Snackbar>
